53955b3998e909791c2b89d5c68275c45d004989
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-02-14 12:24:00 +0100

    solution 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
-import './globals.css'
+import './globals.css';
+import Providers from './providers';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       {/*
@@ -12,7 +9,9 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,25 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import { prisma } from '~/src/db/prisma';
+
+if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET) {
+  throw new Error(
+    'Please define the GITHUB_ID and GITHUB_SECRET environment variables inside .env.local'
+  );
+}
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
